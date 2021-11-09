c4fa5c311a79e58ccbad34cbde695a4856407421
Tutto Arts and Media  Iwona Wolkowicz <tuttoarts@MacBook-Pro-Tutto.local>
2021-11-09 19:24:31 +0100

    color change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 //import {useState} from 'react'
-import { Suspense }  from 'react';
+import { Suspense, useState }  from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Scene } from './Loader';
 import { Kurek} from './Loader2';
@@ -9,19 +9,37 @@ import { Kurek} from './Loader2';
 
 function App() {
 
- // const [kurektype, setkurektype]=useState('/zawor_kulowy_three_kula3_kurek.glb')
-  const kurektype = '/zawor_kulowy_three_kula3_kurek.glb'
 
+  const kurektype = '/zawor_kulowy_three_kula3_kurek.glb';
+  const kurektypeblue = '/zawor_kulowy_three_kula3_kurek_blue.glb'
+ 
+  const [colorkurek,setColorkurek] = useState(kurektype)
+  const [showA,setShow]=useState(true);
+
+  function changekurek(){
+    if(showA===true){
+      
+      setShow(false);
+    } else {
+      setShow(true)
+    }
+  }
+  console.log(colorkurek);
   return (
+    <div>
     <Canvas>
       <Suspense fallback={null}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
 
-      <Scene />
-      <Kurek color={kurektype} />
+      <Scene onClick={(event)=>setColorkurek(kurektypeblue)} />
+      <Kurek color={colorkurek} />
+      {showA&&<Kurek color={kurektypeblue} />}
       </Suspense>
     </Canvas>
+    <button id={'buttonkurek'} onClick={(event)=>changekurek()}>Color</button>
+    </div>
+    
   );
 }
 
